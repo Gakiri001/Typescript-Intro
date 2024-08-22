bfb26dcc1af6a5b8ce9b2c559985996cd337b5a8
Gakiri001 <gakirijulius@gmail.com>
2024-08-22 10:31:02 +0300

    TypeScript Intro -> DataTypes, Functions

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+"use strict";
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let age = [20, 21, 22];
+let user = [1, "Julius"];
+console.log(user[0].toString);
+console.log(user[1].length);
+user.push(1);
+let mySize = 2;
+console.log(mySize);
+function CalculateTax(income, taxYear) {
+    if ((taxYear || 2022) < 2022) {
+        return income * 1.2;
+    }
+    return income * 1.3;
+}
+console.log(CalculateTax(10000, 2023));
+let employee = {
+    id: 1,
+    name: "Julius",
+    department: "Accounts"
+};
+console.log(employee);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+let sales = 123_456_789;
+let course = "TypeScript"
+let is_published: boolean = true;
+
+//any
+let level;
+
+function render(document: any){
+  console.log(document)
+}
+
+//Array
+let numbers = [1,2,3]
+let age: number[] = [20,21,22]
+
+// tuple
+let user: [number, string] = [1,"Julius"]
+console.log(user[0].toString) //access the first dataType in the tuple
+console.log(user[1].length)//access the second dataType in the tuple
+user.push(1)
+
+//Enums
+// Use PascalCase - The 1st Char shld be capital
+const enum Size {Small = 1, Medium, Large}
+let mySize: Size = Size.Medium
+console.log(mySize)
+
+
+//Functions -> Always annotate your function to specify the return value.
+function CalculateTax(income: number, taxYear?: number): number{
+  if ((taxYear || 2022) < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3
+}
+
+console.log(CalculateTax(10000,2023))
+
+
+//Objects
+let employee: {id: number, name: string, department: string} = {
+  id: 1,
+  name: "Julius",
+  department: "Accounts"
+}
+
+console.log(employee)
+
